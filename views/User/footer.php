3bd6f151be07cd45c38b2667228873f36c37f770
Elgi Avilla Yandana <elgitheedge@gmail.com>
2018-01-26 13:12:14 +0700

    delete views

--- views/User/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="page-footer" style="background-color: #0c1a1e;">
-    <div>
-      
-    </div>
-    <div class="footer-copyright black">
-      <div class="container" style="text-align: center;">© 2017 Dollak Indonesia</div>
-    </div>
-  </footer>
